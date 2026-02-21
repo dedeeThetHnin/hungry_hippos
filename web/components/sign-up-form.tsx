5fb2c1db2ae13fb9e0fed9e99f0e6bfac69c37b3
Daniel <167367785+u-iDaniel@users.noreply.github.com>
2026-02-20 22:29:49 -0700

    Redirect on login page if signed in already

--- web/components/sign-up-form.tsx
+++ web/components/sign-up-form.tsx
@@ -53,7 +53,7 @@ export function SignUpForm({
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${window.location.origin}/dashboard`,
+        redirectTo: `${window.location.origin}/auth/callback?next=/dashboard`,
       },
     });
   };
